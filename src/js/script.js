d155844d0d360676e60d8ef490d06345d6bd6573
Mingyue Sun <mingyueees@gmail.com>
2020-12-11 17:00:09 -0800

    Slider finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const closeIcon = document.querySelector('.close-icon');
+const hamburgerIcon = document.querySelector('.icon-hamburger');
+const closeIcon = document.querySelector('.icon-close');
 const menu = document.querySelector('.nav-links');
+const innerSlider = document.querySelector('.slider-inner');
+const sliderItems = document.querySelectorAll('.slider-item');
+const sliderIndicators = document.querySelectorAll('.btn-num');
 
-// toogle menu
+// toggle menu
 const toggleMenu = () => {
   const isShown = menu.style.display === 'block';
 
@@ -21,4 +24,39 @@ window.addEventListener('resize', () => {
   menu.style.display = isLargeScreen ? 'block' : 'none';
   hamburgerIcon.style.display = isLargeScreen ? 'none' : 'inline';
   closeIcon.style.display = isLargeScreen ? 'inline' : 'none';
+});
+
+// homepage slider
+let currentIndex = 0;
+
+const moveSliderFrom = (slide, button) => {
+  slide.classList.remove('active');
+  button.classList.remove('active');
+};
+
+const moveSliderTo = (slide, button) => {
+  slide.classList.add('active');
+  button.classList.add('active');
+};
+
+setInterval(() => {
+  moveSliderFrom(sliderItems[currentIndex], sliderIndicators[currentIndex]);
+
+  if (currentIndex === 3) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+
+  moveSliderTo(sliderItems[currentIndex], sliderIndicators[currentIndex]);
+}, 5000);
+
+sliderIndicators.forEach(indicator => {
+  indicator.addEventListener('click', () => {
+    moveSliderFrom(sliderItems[currentIndex], sliderIndicators[currentIndex]);
+
+    currentIndex = parseInt(indicator.innerHTML) - 1;
+
+    moveSliderTo(sliderItems[currentIndex], sliderIndicators[currentIndex]);
+  });
 });
