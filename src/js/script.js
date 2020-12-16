0fa8f97ec3832d97616c8b4e1df388f9a80becee
Mingyue Sun <mingyueees@gmail.com>
2020-12-15 23:02:00 -0800

    Contact page finished

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,10 @@ const closeIcon = document.querySelector('.icon-close');
 const menu = document.querySelector('.nav-links');
 const innerSlider = document.querySelector('.slider-inner');
 const sliderItems = document.querySelectorAll('.slider-item');
-const sliderIndicators = document.querySelectorAll('.btn-num');
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const textarea = document.querySelector('textarea');
+const errorMessages = document.querySelectorAll('.error-message');
 
 // toggle menu
 const toggleMenu = () => {
@@ -59,4 +62,36 @@ sliderIndicators.forEach(indicator => {
 
     moveSliderTo(sliderItems[currentIndex], sliderIndicators[currentIndex]);
   });
+});
+
+// form validation
+const inputAndTextarea = [...inputs, textarea];
+
+const isValid = input => input.value !== '';
+
+const generateError = input => {
+  if (!isValid(input)) {
+    input.classList.add('error');
+    input.nextElementSibling.textContent = "Can't be empty";
+  }
+};
+
+const removeError = input => {
+  input.classList.remove('error');
+  input.nextElementSibling.textContent = '';
+};
+
+form.addEventListener('submit', e => {
+  inputAndTextarea.forEach(input => generateError(input));
+
+  for (let i = 0; i < inputAndTextarea.length; i++) {
+    if (inputAndTextarea[i].classList.contains('error')) {
+      e.preventDefault();
+      return;
+    }
+  }
+});
+
+inputAndTextarea.forEach(input => {
+  input.addEventListener('focus', () => removeError(input));
 });
